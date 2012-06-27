abd690e31dae0eb4e337bc84b8ab18a5dc7b230b
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2012-06-27 20:40:01 +0200

    Added assertions and documentation to euclidean_distances

--- src/euclidean_distances.py
+++ src/euclidean_distances.py
@@ -4,6 +4,17 @@ import numpy as np
 def euclidean_distances(X, Y):
     """
     Compute the euclidean distance
+
+    Parameters
+    ----------
+    X: ndarray
+
+    Y: ndarray
+
+    Returns
+    -------
+    distances: float
     """
+    assert (X.shape == Y.shape)
     d = ((X - Y) ** 2).sum()
     return np.sqrt(d)
